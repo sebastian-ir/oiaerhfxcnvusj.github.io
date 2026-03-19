56752a0841f4a6a9ed8ce4fa01ef11013d863a32
Sebastian <sebastian.ir.mail@gmail.com>
2026-03-19 04:19:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const select = document.getElementById('Theme');
+const savedTheme = localStorage.getItem('theme') || 'light';
 
+document.documentElement.setAttribute('data-theme', savedTheme);
+select.value = savedTheme;
+
+select.addEventListener('change', () => {
+    document.documentElement.setAttribute('data-theme', select.value);
+    localStorage.setItem('theme', select.value);
+});
